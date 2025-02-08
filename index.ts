426f33528630b9c29f0007587e6f4c5c965ea604
Luca SK <sarifluca@gmail.com>
2025-02-08 02:36:23 +0000

    ugh

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { ethers } from 'ethers';
 import { readFile } from "fs/promises";
+import {db} from './server';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,7 +14,7 @@ app.get('/', async (req, res) => {
     const { username } = req.body;
 
     res.send('Hello, TypeScript!');
-    const users: string = await readFile('users.txt', 'utf8');
+    const users = await readFile('users.txt', 'utf8');
 
     if (users.includes(username)){
         res.send('Authenticated!');
@@ -69,6 +70,30 @@ app.post("/verify-signature", (req, res) => {
         res.status(500).json({ error: "Signature verification failed", details: error });
     }
 });
+
+
+app.get('/records', async (req, res) => {
+    try {
+      const records = await db.all()
+      res.json({ records })
+    } catch (error) {
+      res.status(500).json({ error: error })
+    }
+  })
+  
+  app.post('/records', async (req, res) => {
+    try {
+      const { value } = req.body
+      if (!value) {
+        res.status(400).json({ error: 'Value is required' })
+      }
+  
+      await db.add(value)
+      res.json({ success: true, message: 'Record added' })
+    } catch (error) {
+      res.status(500).json({ error: error })
+    }
+  })
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-import express from 'express'
-import cors from 'cors'
 import { createLibp2p } from 'libp2p'
 import { createHelia } from 'helia'
 import { createOrbitDB } from '@orbitdb/core'
 import { LevelBlockstore } from 'blockstore-level'
 import { Libp2pOptions } from './config/libp2p.js'
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-
-let db
+export let db;
 
 async function setupDB() {
   const blockstore = new LevelBlockstore('./ipfs/blocks')
@@ -23,29 +17,6 @@ async function setupDB() {
 }
 
 setupDB().catch(console.error)
-
-app.get('/records', async (req, res) => {
-  try {
-    const records = await db.all()
-    res.json({ records })
-  } catch (error) {
-    res.status(500).json({ error: error.message })
-  }
-})
-
-app.post('/records', async (req, res) => {
-  try {
-    const { value } = req.body
-    if (!value) {
-      return res.status(400).json({ error: 'Value is required' })
-    }
-
-    await db.add(value)
-    res.json({ success: true, message: 'Record added' })
-  } catch (error) {
-    res.status(500).json({ error: error.message })
-  }
-})
 
 const PORT = 3000
 app.listen(PORT, () => {
